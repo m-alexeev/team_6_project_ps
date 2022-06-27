e07ddc26fa23277bbf69056d3bcd71da173f89b2
Mikhail Alexeev <mikalexe@publicisgroupe.net>
2022-06-27 13:16:42 -0400

    container

--- src/views/ShoppingList.tsx
+++ src/views/ShoppingList.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import { Instantiator } from "../models/Instantiator";
+import {Container} from 'native-base'
 interface Props{
 
 }
 
 const ShoppingList: React.FC<Props> = () => {
+    Instantiator.createStoreStructure()
+    const items = Instantiator.items;
 
     return(
+        <Container>
 
+        </Container>
     )
 }
 
