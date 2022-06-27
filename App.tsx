13e605c4bdc42542793cfb037c99616b037fcbd8
Mikhail Alexeev <mikalexe@publicisgroupe.net>
2022-06-27 16:10:30 -0400

    fixed not issue with nothing rendering

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ShoppingList from './src/views/ShoppingList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <ShoppingList></ShoppingList>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <ShoppingList></ShoppingList>
+      </View>
+    </NativeBaseProvider>
   );
 }
 
